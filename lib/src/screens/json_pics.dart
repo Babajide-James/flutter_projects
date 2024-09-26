c76fd9f419650761015c694c55735e0ef76bbe11
Babajide-James <teestartaiwo121@gmail.com>
2024-09-27 00:10:50 +0100

    debugged successfully

--- lib/src/screens/json_pics.dart
+++ lib/src/screens/json_pics.dart
@@ -1,13 +1,12 @@
 class JsonPics {
-  late int id;
-  late String title;
-  late String url;
+  int id;
+  String title;
+  String url;
 
   JsonPics(this.id, this.title, this.url);
 
-  JsonPics.fromJson(Map<String, dynamic> passJson) {
-    id = passJson['id'];
-    title = passJson['title'];
-    url = passJson['url'];
-  }
+  JsonPics.fromJson(Map<String, dynamic> passJson)
+      : id = passJson['id'],
+        title = passJson['title'],
+        url = passJson['url'];
 }
