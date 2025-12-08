9d04bf93497b8ce779237dc4c5d681f418a13783
Babajide James <teestartaiwo121@gmail.com>
2025-12-08 23:06:40 +0100

    added filter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/dummies.dart';
 import './screens/ingredient_screen.dart';
 import '../screens/home_screen.dart';
 import 'screens/filter_route.dart';
 import 'widgets/bottom_nav.dart';
 import '../screens/food_screen.dart';
+import './modal/modal.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +17,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Map<String, bool> filters = {
+    'vegan': false,
+    'glutenFree': false,
+    'vegetarian': false,
+    'lactoseFree': false,
+  };
+  List<FoodModal> setFilters = FOOD_DUMMIES;
+  void filterSetting(Map<String, bool> filterData) {
+    setState(() {
+      filters = filterData;
+
+      setFilters = FOOD_DUMMIES.where((food) {}).toList();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -25,16 +42,16 @@ class _MyAppState extends State<MyApp> {
         fontFamily: 'Raleway',
         scaffoldBackgroundColor: const Color.fromARGB(235, 243, 242, 231),
         textTheme: ThemeData.light().textTheme.copyWith(
-          bodyLarge: TextStyle(fontFamily: 'RobotoCondensed', fontSize: 28),
+          bodyLarge: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
       // home:  HomeScreen(),
       initialRoute: '/',
       routes: {
         '/': (ctx) => BottomNav(),
-        FoodScreen.routeName: (ctx) => FoodScreen(),
+        FoodScreen.routeName: (ctx) => FoodScreen(setFilters),
         IngredientScreen.routeName: (ctx) => IngredientScreen(),
-        Filter.routeName: ((ctx) => Filter()),
+        Filter.routeName: ((ctx) => Filter(filterSetting)),
       },
       //       onGenerateRoute: (settings){
       // MaterialPageRoute(builder: (ctx)=> HomeScreen());
--- lib/screens/filter_route.dart
+++ lib/screens/filter_route.dart
@@ -1,15 +1,113 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class Filter extends StatelessWidget {
-  const Filter({super.key});
+class Filter extends StatefulWidget {
+  const Filter({super.key, required this.saveFilters});
   static const routeName = '/favorite_route';
+  final Function saveFilters;
+
+  @override
+  State<Filter> createState() => _FilterState();
+}
+
+class _FilterState extends State<Filter> {
+  late Function onChange;
+  var _isGlutenFree = false;
+  var _isVegan = false;
+  var _isVegetarian = false;
+  var _isLactoseFree = false;
+
+  Widget filterSwitch(
+    String title,
+    String subtitle,
+    var value,
+    ValueChanged<bool> changeValue,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: changeValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('')),
+      appBar: AppBar(
+        title: Text('Your Filters'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.done),
+            onPressed: () {
+              final selectedFilter = {
+                'vegan': _isVegan,
+                'gluten': _isGlutenFree,
+                'vegetarian': _isVegetarian,
+                'lactose': _isLactoseFree,
+              };
+              widget.saveFilters(selectedFilter);
+            },
+          ),
+        ],
+      ),
       drawer: MainDrawer(),
-      body: Center(child: Text('Our New Favorites')),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            // color: Colors.black,
+            width: double.infinity,
+            child: Text(
+              'Adjust your meal selection',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          filterSwitch(
+            'Gluten-Free',
+            'This Food is Gluten-Free',
+            _isGlutenFree,
+            (newValue) {
+              setState(() {
+                _isGlutenFree = newValue;
+              });
+            },
+          ),
+          filterSwitch('Vegan-Free', 'choose only vegan-free meal', _isVegan, (
+            newValue,
+          ) {
+            setState(() {
+              _isVegan = newValue;
+            });
+          }),
+          filterSwitch(
+            'Vegetarian',
+            'Choose only Vegeterian Meal',
+            _isVegetarian,
+            (newValue) {
+              setState(() {
+                _isVegetarian = newValue;
+              });
+            },
+          ),
+          filterSwitch(
+            'Lactose-Free',
+            'Choose Meal that are Lactose-Free',
+            _isLactoseFree,
+            (newValue) {
+              setState(() {
+                _isLactoseFree = newValue;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
